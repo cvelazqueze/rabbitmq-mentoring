2d92ddd273676482d7da67227efc95fec43ad4d2
Cesar Velazquez <cesar.velazquez@runahr.com>
2021-10-29 16:17:10 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var amqp = require("amqplib/callback_api");
+
+
+var app = express();
+
+app.listen(3000, () => {
+    console.log ("Server running")
+});
+
+app.get("/", (req, res, next) =>  {
+    res.json("Hello wold!");
+    amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {
+            noAck: true
+        });
+    });
+});
+})
+
+app.get("/send", (req, res, next) => {
+    res.json("Message Sent");
+    amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+        var msg = 'Hello World from the publisher!';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
+    });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
+})
